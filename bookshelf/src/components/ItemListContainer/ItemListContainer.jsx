fadfcf8e0cc1d67f2a4656a53e6182bf11d6b4ea
Daniel Adamis <daniel_adamis@hotmail.com>
2024-03-25 02:36:02 -0300

    Added CartWidget & ItemListContainer components; fixed project structure

---
+++ bookshelf/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import './../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Cart } from 'react-bootstrap-icons';
+
+function ItemListContainer() {
+    return (
+        <Cart color='orange' />
+    );
+}
+
+export default ItemListContainer;
